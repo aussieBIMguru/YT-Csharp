988476b394118b82373d4b86f0510a6b99457a63
Gavin Crump <aussiebimguru@gmail.com>
2025-03-01 19:45:01 +1030

    Lesson 1

---
+++ guRoo lessons/Lesson 1/guRoo/guRoo/Application.cs
@@ -0,0 +1,26 @@
+﻿using guRoo.Commands;
+using Nice3point.Revit.Toolkit.External;
+
+namespace guRoo
+{
+    /// <summary>
+    ///     Application entry point
+    /// </summary>
+    [UsedImplicitly]
+    public class Application : ExternalApplication
+    {
+        public override void OnStartup()
+        {
+            CreateRibbon();
+        }
+
+        private void CreateRibbon()
+        {
+            var panel = Application.CreatePanel("Commands", "guRoo");
+
+            panel.AddPushButton<StartupCommand>("Execute")
+                .SetImage("/guRoo;component/Resources/Icons/RibbonIcon16.png")
+                .SetLargeImage("/guRoo;component/Resources/Icons/RibbonIcon32.png");
+        }
+    }
+}
---
+++ guRoo lessons/Lesson 1/guRoo/guRoo/Commands/StartupCommand.cs
@@ -0,0 +1,21 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI;
+using Nice3point.Revit.Toolkit.External;
+
+namespace guRoo.Commands
+{
+    /// <summary>
+    ///     External command entry point invoked from the Revit interface
+    /// </summary>
+    [UsedImplicitly]
+    [Transaction(TransactionMode.Manual)]
+    public class StartupCommand : ExternalCommand
+    {
+        public override void Execute()
+        {
+            TaskDialog.Show(Document.Title, "Hot reload!");
+
+            int testing = 1;
+        }
+    }
+}
