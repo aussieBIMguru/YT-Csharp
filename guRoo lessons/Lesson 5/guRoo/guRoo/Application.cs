6dc36a930b0bffbb294e77fa896eaad16dfe2ab5
Gavin Crump <aussiebimguru@gmail.com>
2025-03-16 14:12:42 +1030

    Lesson 5

---
+++ guRoo lessons/Lesson 5/guRoo/guRoo/Application.cs
@@ -0,0 +1,31 @@
+﻿// Autodesk
+using Autodesk.Revit.UI;
+
+// This application belongs to the root namespace
+namespace guRoo
+{
+    // Implementing the interface for applications
+    public class Application: IExternalApplication
+    {
+        // This will run on startup
+        public Result OnStartup(UIControlledApplication uiCtlApp)
+        {
+            // Collect the controlled application
+            var ctlApp = uiCtlApp.ControlledApplication;
+            
+            // Code logic here
+
+            // Final return
+            return Result.Succeeded;
+        }
+
+        // This will run on shutdown
+        public Result OnShutdown(UIControlledApplication uiCtlApp)
+        {
+            // Cleanup code logic
+            
+            // Final return
+            return Result.Succeeded;
+        }
+    }
+}
