f8dc9abce02f871f6d1e919c57f2969966cfa7ca
Gavin Crump <aussiebimguru@gmail.com>
2025-03-30 13:43:29 +1030

    Lesson 11

---
+++ guRoo lessons/Lesson 11/guRoo/guRoo/Commands/General/Cmds_PullDown.cs
@@ -0,0 +1,70 @@
+﻿// Autodesk
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI;
+
+// Associate with PullDown example commands
+namespace guRoo.Cmds_PullDown
+{
+    /// <summary>
+    /// Example command
+    /// </summary>
+    [Transaction(TransactionMode.Manual)]
+    public class Cmd_1Button : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // Collect the document
+            var uiApp = commandData.Application;
+            var uiDoc = uiApp.ActiveUIDocument;
+            var doc = uiDoc.Document;
+
+            // Testing if the command worked
+            TaskDialog.Show("Button 1 is working!", doc.Title);
+
+            // Final return
+            return Result.Succeeded;
+        }
+    }
+
+    /// <summary>
+    /// Example command
+    /// </summary>
+    [Transaction(TransactionMode.Manual)]
+    public class Cmd_2Button : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // Collect the document
+            var uiApp = commandData.Application;
+            var uiDoc = uiApp.ActiveUIDocument;
+            var doc = uiDoc.Document;
+
+            // Testing if the command worked
+            TaskDialog.Show("Button 2 is working!", doc.Title);
+
+            // Final return
+            return Result.Succeeded;
+        }
+    }
+
+    /// <summary>
+    /// Example command
+    /// </summary>
+    [Transaction(TransactionMode.Manual)]
+    public class Cmd_3Button : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // Collect the document
+            var uiApp = commandData.Application;
+            var uiDoc = uiApp.ActiveUIDocument;
+            var doc = uiDoc.Document;
+
+            // Testing if the command worked
+            TaskDialog.Show("Button 3 is working!", doc.Title);
+
+            // Final return
+            return Result.Succeeded;
+        }
+    }
+}
