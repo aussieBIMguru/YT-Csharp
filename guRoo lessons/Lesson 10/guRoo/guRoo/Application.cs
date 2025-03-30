8e9a7e1d8adfc5215ba9d13cece3d87d076b5f85
Gavin Crump <aussiebimguru@gmail.com>
2025-03-30 13:13:19 +1030

    Lesson 10

---
+++ guRoo lessons/Lesson 10/guRoo/guRoo/Application.cs
@@ -0,0 +1,106 @@
+﻿// Autodesk
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Events;
+// guRoo
+using guRoo.Extensions;
+
+// This application belongs to the root namespace
+namespace guRoo
+{
+    // Implementing the interface for applications
+    public class Application: IExternalApplication
+    {
+        #region Properties
+
+        // Make a private uiCtlApp
+        private static UIControlledApplication _uiCtlApp;
+
+        #endregion
+
+        #region On startup method
+
+        // This will run on startup
+        public Result OnStartup(UIControlledApplication uiCtlApp)
+        {
+            #region Globals registration
+
+            // Store _uiCtlApp, register on idling
+            _uiCtlApp = uiCtlApp;
+
+            try
+            {
+                _uiCtlApp.Idling += RegisterUiApp;
+            }
+            catch
+            {
+                Globals.UiApp = null;
+                Globals.UsernameRevit = null;
+            }
+            
+            // Registering globals
+            Globals.RegisterProperties(uiCtlApp);
+            Globals.RegisterTooltips($"{Globals.AddinName}.Resources.Files.Tooltips");
+
+            #endregion
+
+            #region Ribbon setup
+
+            // Add ribbon tab
+            uiCtlApp.Ext_AddRibbonTab(Globals.AddinName);
+
+            // Create panel
+            var panelGeneral = uiCtlApp.Ext_AddRibbonPanel(Globals.AddinName, "General");
+
+            // Add button to panel
+            var buttonTest = panelGeneral.Ext_AddPushButton("Button", "guRoo.Cmds_Button.Cmd_Button");
+
+            // Add pulldownbutton to panel
+            var pulldownTest = panelGeneral.Ext_AddPulldownButton("PullDown", "guRoo.Cmds_PullDown");
+
+            // Add buttons to pulldown
+            pulldownTest.Ext_AddPushButton("Button 1", "guRoo.Cmds_PullDown.Cmd_1Button");
+            pulldownTest.Ext_AddPushButton("Button 2", "guRoo.Cmds_PullDown.Cmd_2Button");
+            pulldownTest.Ext_AddPushButton("Button 3", "guRoo.Cmds_PullDown.Cmd_3Button");
+
+            #endregion
+
+            // Final return
+            return Result.Succeeded;
+        }
+
+        #endregion
+
+        #region On shutdown method
+
+        // This will run on shutdown
+        public Result OnShutdown(UIControlledApplication uiCtlApp)
+        {
+            // Cleanup code logic
+            
+            // Final return
+            return Result.Succeeded;
+        }
+
+        #endregion
+
+        #region Use idling to register UiApp
+
+        /// <summary>
+        /// Registers the UiApp and Revit username globally.
+        /// </summary>
+        /// <param name="sender">Sender of the Idling event.</param>
+        /// <param name="e">Idling event arguments.</param>
+        private static void RegisterUiApp(object sender, IdlingEventArgs e)
+        {
+            _uiCtlApp.Idling -= RegisterUiApp;
+
+            if (sender is UIApplication uiApp)
+            {
+                Globals.UiApp = uiApp;
+                Globals.UsernameRevit = uiApp.Application.Username;
+            }
+        }
+
+        #endregion
+    }
+}
