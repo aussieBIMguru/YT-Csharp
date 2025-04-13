aafde6d4b71c5f49cd8bfdf4333e00b597abd36a
Gavin Crump <aussiebimguru@gmail.com>
2025-04-13 16:47:32 +0930

    Lesson 14

---
+++ guRoo lessons/Lesson 14/guRoo/Commands/General/Cmds_Button.cs
@@ -0,0 +1,34 @@
+﻿// Autodesk
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI;
+// guRoo
+using guRoo.Extensions;
+using gFrm = guRoo.Forms;
+
+// Associate with pushbutton commands
+namespace guRoo.Cmds_Button
+{
+    /// <summary>
+    /// Example command
+    /// </summary>
+    [Transaction(TransactionMode.Manual)]
+    public class Cmd_Button: IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // Collect the document
+            var uiApp = commandData.Application;
+            var uiDoc = uiApp.ActiveUIDocument;
+            var doc = uiDoc.Document;
+
+            // Select a revision
+            var revisionResult = doc.Ext_SelectRevision();
+            if (revisionResult.Cancelled) { return Result.Cancelled; }
+            var selectedRevision = revisionResult.Object as Revision;
+
+            // Test the outcome
+            gFrm.Custom.Message(message: selectedRevision.Ext_ToRevisionKey());
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ guRoo lessons/Lesson 14/guRoo/Utilities/Ribbon_Utils.cs
@@ -0,0 +1,133 @@
+﻿// System
+using Autodesk.Revit.UI;
+using System.Diagnostics;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+// Associate to the utility namespace
+namespace guRoo.Utilities
+{
+    // These utilites relate to ribbon creation/management
+    public static class Ribbon_Utils
+    {
+        #region Button data
+
+        /// <summary>
+        /// Create a PushButtonData object.
+        /// </summary>
+        /// <param name="buttonName">The name the user sees.</param>
+        /// <param name="className">The full class name the button runs.</param>
+        /// <returns>A PushButtonData object.</returns>
+        public static PushButtonData NewPushButtonData(string buttonName, string className)
+        {
+            // Get our base name
+            var baseName = CommandToBaseName(className);
+
+            // Create a data object
+            var pushButtonData = new PushButtonData(baseName, buttonName, Globals.AssemblyPath, className);
+
+            // Set the values
+            pushButtonData.ToolTip = LookupTooltip(baseName);
+            pushButtonData.Image = GetIcon(baseName, resolution: 16);
+            pushButtonData.LargeImage = GetIcon(baseName, resolution: 32);
+
+            // Return the object
+            return pushButtonData;
+        }
+
+        /// <summary>
+        /// Create a PulldownButtonData object.
+        /// </summary>
+        /// <param name="buttonName">The name the user sees.</param>
+        /// <param name="className">The full class name the button runs.</param>
+        /// <returns>A PulldownButtonData object.</returns>
+        public static PulldownButtonData NewPulldownButtonData(string buttonName, string className)
+        {
+            // Get our base name
+            var baseName = CommandToBaseName(className);
+
+            // Create a data object
+            var pulldownButtonData = new PulldownButtonData(baseName, buttonName);
+
+            // Set the values
+            pulldownButtonData.ToolTip = LookupTooltip(baseName);
+            pulldownButtonData.Image = GetIcon(baseName, resolution: 16);
+            pulldownButtonData.LargeImage = GetIcon(baseName, resolution: 32);
+
+            // Return the object
+            return pulldownButtonData;
+        }
+
+        #endregion
+
+        #region Resource management
+
+        /// <summary>
+        /// Converts a commnand name to the base name standard.
+        /// </summary>
+        /// <param name="commandName">The command class name.</param>
+        /// <returns>A string (the base name).</returns>
+        public static string CommandToBaseName(string commandName)
+        {
+            return commandName.Replace("guRoo.Cmds_", "").Replace(".Cmd", "");
+        }
+
+        /// <summary>
+        /// Looks up a tooltip by key from the Global tooltips.
+        /// </summary>
+        /// <param name="key">The key of the tooltip.</param>
+        /// <param name="failValue">Default value to return.</param>
+        /// <returns></returns>
+        public static string LookupTooltip(string key, string failValue = null)
+        {
+            // Null coalesce the tooltip
+            failValue ??= "No tooltip was found.";
+
+            // Try to find the tooltip value
+            if (Globals.Tooltips.TryGetValue(key, out string value))
+            {
+                return value;
+            }
+
+            // Return the default value if it failed
+            return failValue;
+        }
+
+        /// <summary>
+        /// Return the icon resource as an ImageSource.
+        /// </summary>
+        /// <param name="baseName">The base name of the icon resource.</param>
+        /// <param name="resolution">The icon resolution to find.</param>
+        /// <returns></returns>
+        public static ImageSource GetIcon(string baseName, int resolution = 32)
+        {
+            // Get the path to the resource
+            var resourcePath = $"{Globals.AddinName}.Resources.Icons{resolution}.{baseName}{resolution}.png";
+
+            // Stream the resource
+            using (var stream = Globals.Assembly.GetManifestResourceStream(resourcePath))
+            {
+                // Return null if no stream found
+                if (stream is null) { return null; }
+
+                // Convert the resource to a bitmap
+                var decoder = new PngBitmapDecoder(
+                    stream, 
+                    BitmapCreateOptions.PreservePixelFormat,
+                    BitmapCacheOption.Default);
+
+                // Return the first frame if available
+                if (decoder.Frames.Count > 0)
+                {
+                    return decoder.Frames.First();
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
