144cd7b036d88c2abe001b9d29f96b6b0a3e9037
Gavin Crump <aussiebimguru@gmail.com>
2025-04-18 18:58:38 +0930

    Lesson 20

---
+++ guRoo lessons/Lesson 20/guRoo/guRoo/Application.cs
@@ -0,0 +1,142 @@
+﻿// Autodesk
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Events;
+// guRoo
+using guRoo.Extensions;
+using gRib = guRoo.Utilities.Ribbon_Utils;
+
+// This application belongs to the root namespace
+namespace guRoo
+{
+    // Implementing the interface for applications
+    public class Application: IExternalApplication
+    {
+        #region Properties
+
+        // Make a private uiCtlApp
+        private static UIControlledApplication _uiCtlApp;
+
+        #endregion
+
+        #region On startup method
+
+        // This will run on startup
+        public Result OnStartup(UIControlledApplication uiCtlApp)
+        {
+            #region Globals registration
+
+            // Store _uiCtlApp, register on idling
+            _uiCtlApp = uiCtlApp;
+
+            try
+            {
+                _uiCtlApp.Idling += RegisterUiApp;
+            }
+            catch
+            {
+                Globals.UiApp = null;
+                Globals.UsernameRevit = null;
+            }
+            
+            // Registering globals
+            Globals.RegisterProperties(uiCtlApp);
+            Globals.RegisterTooltips($"{Globals.AddinName}.Resources.Files.Tooltips");
+
+            #endregion
+
+            #region Ribbon setup examples
+
+            // Add ribbon tab
+            uiCtlApp.Ext_AddRibbonTab(Globals.AddinName);
+
+            // Create panel
+            var panelGeneral = uiCtlApp.Ext_AddRibbonPanel(Globals.AddinName, "General");
+
+            // Add button to panel
+            var buttonTest = panelGeneral.Ext_AddPushButton("Button", "guRoo.Cmds_Button.Cmd_Button");
+
+            // Add pulldownbutton to panel
+            var pulldownTest = panelGeneral.Ext_AddPulldownButton("PullDown", "guRoo.Cmds_PullDown");
+
+            // Add buttons to pulldown
+            pulldownTest.Ext_AddPushButton("Button 1", "guRoo.Cmds_PullDown.Cmd_1Button");
+            pulldownTest.Ext_AddPushButton("Button 2", "guRoo.Cmds_PullDown.Cmd_2Button");
+            pulldownTest.Ext_AddPushButton("Button 3", "guRoo.Cmds_PullDown.Cmd_3Button");
+
+            // Create data objects for the stack
+            var stack1Data = gRib.NewPulldownButtonData("Stack 1", "guRoo.Cmds_Stack1");
+            var stack2Data = gRib.NewPulldownButtonData("Stack 2", "guRoo.Cmds_Stack2");
+            var stack3Data = gRib.NewPulldownButtonData("Stack 3", "guRoo.Cmds_Stack3");
+
+            // Create the stack
+            var stack = panelGeneral.AddStackedItems(stack1Data, stack2Data, stack3Data);
+            var pulldownStack1 = stack[0] as PulldownButton;
+            var pulldownStack2 = stack[1] as PulldownButton;
+            var pulldownStack3 = stack[2] as PulldownButton;
+
+            // Add buttons to stacked pulldowns
+            pulldownStack1.Ext_AddPushButton("Button", "guRoo.Cmds_Stack1.Cmd_Button");
+            pulldownStack2.Ext_AddPushButton("Button", "guRoo.Cmds_Stack2.Cmd_Button");
+            pulldownStack3.Ext_AddPushButton("Button", "guRoo.Cmds_Stack3.Cmd_Button");
+
+            #endregion
+
+            #region Command examples
+
+            // Create tools panel
+            var panelTools = uiCtlApp.Ext_AddRibbonPanel(Globals.AddinName, "Tools");
+
+            // Add revision pulldown
+            var pulldownRevision = panelTools.Ext_AddPulldownButton("Revision", "guRoo.Cmds_Revision");
+
+            // Add pushbuttons to revision
+            pulldownRevision.Ext_AddPushButton("BulkRev", "guRoo.Cmds_Revision.Cmd_BulkRev");
+
+            // Add worksets pulldown
+            var pulldownWorkset = panelTools.Ext_AddPulldownButton("Workset", "guRoo.Cmds_Workset");
+
+            // Add pushbuttons to workset
+            pulldownWorkset.Ext_AddPushButton("Create workset(s)", "guRoo.Cmds_Workset.Cmd_Create");
+
+            #endregion
+
+            // Final return
+            return Result.Succeeded;
+        }
+
+        #endregion
+
+        #region On shutdown method
+
+        // This will run on shutdown
+        public Result OnShutdown(UIControlledApplication uiCtlApp)
+        {
+            // Cleanup code logic
+            
+            // Final return
+            return Result.Succeeded;
+        }
+
+        #endregion
+
+        #region Use idling to register UiApp
+
+        /// <summary>
+        /// Registers the UiApp and Revit username globally.
+        /// </summary>
+        /// <param name="sender">Sender of the Idling event.</param>
+        /// <param name="e">Idling event arguments.</param>
+        private static void RegisterUiApp(object sender, IdlingEventArgs e)
+        {
+            _uiCtlApp.Idling -= RegisterUiApp;
+
+            if (sender is UIApplication uiApp)
+            {
+                Globals.UiApp = uiApp;
+                Globals.UsernameRevit = uiApp.Application.Username;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ guRoo lessons/Lesson 20/guRoo/guRoo/Commands/Tools/Cmds_Workset.cs
@@ -0,0 +1,137 @@
+﻿// Autodesk
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI;
+// guRoo
+using guRoo.Extensions;
+using gFrm = guRoo.Forms;
+
+namespace guRoo.Cmds_Workset
+{
+    /// <summary>
+    /// Example command
+    /// </summary>
+    [Transaction(TransactionMode.Manual)]
+    public class Cmd_Create : IExternalCommand
+    {
+        private static readonly List<string> WorksetNames = new List<string>()
+        {
+            "AR Interior",
+            "AR Facade",
+            "AR Structure",
+            "AR Fitout",
+            "AR Site",
+            "Z_Link DWG XXX",
+            "Z_Link RVT XXX"
+        };
+
+        private static readonly string GridsWorksetName = "AR Levels and Grids";
+        
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // Collect the document
+            var uiApp = commandData.Application;
+            var uiDoc = uiApp.ActiveUIDocument;
+            var doc = uiDoc.Document;
+
+            // Make sure the document is workshared
+            if (!doc.IsWorkshared)
+            {
+                return gFrm.Custom.Cancelled("Document is not workshared.");
+            }
+
+            // Get worksets and names
+            var worksets = doc.Ext_GetWorksets();
+            var worksetNames = worksets.Select(w => w.Name).ToList();
+
+            // Get workset names that are not present
+            var unusedNames = WorksetNames.Where(n => !worksetNames.Contains(n)).ToList();
+
+            // Cancel if no missing worksets
+            if (unusedNames.Count == 0)
+            {
+                return gFrm.Custom.Cancelled("All standard worksets are available.");
+            }
+
+            // Choose the missing names from a list
+            var formResult = gFrm.Custom.SelectFromList(keys: unusedNames,
+                values: unusedNames.Cast<object>().ToList(),
+                title: "Select worksets to add to document");
+            if (formResult.Cancelled) { return Result.Cancelled; }
+            var chosenWorksetNames = formResult.Objects.Cast<string>().ToList();
+
+            // Offer to rename grids workset if present
+            if (worksetNames.Contains("Shared Levels and Grids"))
+            {
+                // Ask for renaming
+                var confirmRenameResult = gFrm.Custom.Message(title: "Rename workset",
+                    message: "The default grids and levels workset has been found\n\n" +
+                    "Would you like to rename it?",
+                    yesNo: true);
+
+                // Check if we said yes
+                if (confirmRenameResult.Affirmative)
+                {
+                    // Get the workset
+                    int ind = worksetNames.IndexOf("Shared Levels and Grids");
+                    var gridsWorkset = worksets[ind];
+
+                    // If we can edit it
+                    if (gridsWorkset.IsEditable)
+                    {
+                        // Rename it
+                        using (var t = new Transaction(doc, "guRoo: Rename grids workset"))
+                        {
+                            t.Start();
+
+                            WorksetTable.RenameWorkset(doc, gridsWorkset.Id, GridsWorksetName);
+
+                            t.Commit();
+                        }
+                    }
+                    // Notify if we can't
+                    else
+                    {
+                        gFrm.Custom.Error("The workset was not editable.\n\n" +
+                            "Remaining worksets will now be created anyway.");
+                    }
+                }
+            }
+
+            // Get progress bar properties
+            int progressTotal = chosenWorksetNames.Count;
+            int progressStep = gFrm.Utilities.ProgressDelay(progressTotal);
+
+            // Using a progres bar...
+            using (var pb = new gFrm.ProgressBar(taskName: "Creating worksets", pbTotal: progressTotal))
+            {
+                // Using a transaction...
+                using (var t = new Transaction(doc, "guRoo: Create worksets"))
+                {
+                    t.Start();
+
+                    // Add the worksets
+                    foreach (var name in chosenWorksetNames)
+                    {
+                        // Check for cancellation
+                        if (pb.CancelCheck(t))
+                        {
+                            return Result.Cancelled;
+                        }
+
+                        // Create the workset
+                        Workset.Create(doc, name);
+
+                        // Increment progress
+                        Thread.Sleep(progressStep);
+                        pb.Increment();
+                    }
+
+                    pb.Commit(t);
+                }
+            }
+
+            // Final message to user
+            return gFrm.Custom.Completed($"{progressTotal} workset(s) created");
+        }
+    }
+}
